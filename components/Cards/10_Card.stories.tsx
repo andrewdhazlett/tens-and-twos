fc6b1d8f22af03e7ace5aef91154703cdf74a8fa
andrewdhazlett <andrew@hazlett.me>
2020-12-24 13:31:35 -0600

    add react cards and make typescript

---
+++ components/Cards/10_Card.stories.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { 
+	DealSomeCards,
+	PickACard,
+	Overlay,
+} from ".";
+
+export default {
+  title: "React Playing Cards"
+};
+
+
+export const Example_Poker_Hand = () => <DealSomeCards />;
+export const Overlay_On_Anything = () => <Overlay />;
+export const Pick_a_card = () => <PickACard />;
---
+++ components/Cards/animation/flipCard.ts
@@ -0,0 +1,56 @@
+import { gsap } from "gsap"
+
+let duration = 1;
+
+const setToHidden = (card, callback) => {
+	 gsap.set(card, {
+        rotationY: 90,
+        onComplete: callback,
+    });
+}
+
+const setToShown = (card, callback) => {
+     gsap.set(card, {
+        rotationY: 0,
+        onComplete: callback,
+    });
+}
+
+
+const hide = (card, callback) => {
+     gsap.to(card, {
+        duration: 0.5 * duration,
+        rotationY: -90,
+        onComplete: callback,
+    });
+}
+
+const show = (card, callback) => {
+	 gsap.to(card, {
+        duration: 0.5 * duration,
+        rotationY: 0,
+        onComplete: callback,
+    });
+}
+
+export const flipCard = (animation, card, callback = () => {}) => {
+    switch (animation) {
+        
+
+        case `setToShown`:
+            return setToShown(card, callback)
+
+        case `setToHidden`:
+            return setToHidden(card, callback)
+
+        case `hide`:
+            return hide(card, callback)
+
+        case `show`:
+            return show(card, callback)
+
+        default: {
+            return null
+        }
+    }
+}
